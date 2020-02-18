63f0fea249ed3eee403b767d93350039dfe72cce
indre101 <zygaityte.indre@gmail.com>
2020-02-18 16:43:18 +0100

    displays main color values

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const HTML = {}
-window.addEventListener("DOMContentLoaded", init)
+const HTML = {};
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   HTML.colorInput = document.querySelector(".colorInput");
@@ -7,38 +7,53 @@ function init() {
   HTML.mainColorBlock = document.querySelector(".mainColor .color");
   HTML.rgbColor = document.querySelector(".mainColor .rgbColor");
   HTML.hslColor = document.querySelector(".mainColor .hslColor");
-  HTML.colorInput.addEventListener("input", displayColorValues);
+  HTML.colorInput.addEventListener("input", getColorInputValue);
 }
 
+function getColorInputValue() {
+  let colorCode = event.target.value;
+  displayColorValues(colorCode);
+}
 
-function displayColorValues() {
-  let colorCode = (event.target).value
-  HTML.hexColorCode.textContent = "HEX: " + colorCode
-  HTML.rgbColor.textContent = "RGB: " + hextToRGB(colorCode).join(", ")
-  HTML.hslColor.textContent = "HSL: " + getHSlColor(hextToRGB(colorCode));
+function displayColorValues(colorCode) {
+  const rgb = hextToRGB(colorCode);
+  const hsl = getHSlColor(colorCode);
+  HTML.hexColorCode.textContent = "HEX: " + colorCode;
+  HTML.rgbColor.textContent = "RGB: " + `(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+  HTML.hslColor.textContent = `HSL: (${hsl.h}, ${hsl.s}%, ${hsl.l}% )`;
   HTML.mainColorBlock.style.backgroundColor = colorCode;
 }
 
-function getHSlColor(convertedArrToRGbValues) {
-  r = convertedArrToRGbValues[0];
-  g = convertedArrToRGbValues[1];
-  b = convertedArrToRGbValues[2];
-  return rgbToHsl(r, g, b)
+function getHSlColor(colorCode) {
+  r = hextToRGB(colorCode).r;
+  g = hextToRGB(colorCode).g;
+  b = hextToRGB(colorCode).b;
+  hsl = rgbToHsl(r, g, b);
+  return hsl;
 }
 
+function hextToRGB(colorCode) {
+  let hexColorSeparated = [
+    colorCode.substring(1, 3),
+    colorCode.substring(3, 5),
+    colorCode.substring(5, 7)
+  ];
 
-function hextToRGB(hexColorValue) {
-  let hexColorSeparated = [hexColorValue.substring(1, 3), hexColorValue.substring(3, 5), hexColorValue.substring(5, 7)]
-  const convertedArrToRGbValues = hexColorSeparated.map(characterPair => parseInt(characterPair, 16));
-  return convertedArrToRGbValues;
+  const convertedArrToRGbValues = hexColorSeparated.map(characterPair =>
+    parseInt(characterPair, 16)
+  );
+
+  const rgb = {};
+  rgb.r = convertedArrToRGbValues[0];
+  rgb.g = convertedArrToRGbValues[1];
+  rgb.b = convertedArrToRGbValues[2];
+  return rgb;
 }
 
 function rgbToHsl(r, g, b) {
   r /= 255;
   g /= 255;
   b /= 255;
-
-
   let h, s, l;
 
   const min = Math.min(r, g, b);
@@ -46,14 +61,11 @@ function rgbToHsl(r, g, b) {
 
   if (max === min) {
     h = 0;
-  } else
-  if (max === r) {
+  } else if (max === r) {
     h = 60 * (0 + (g - b) / (max - min));
-  } else
-  if (max === g) {
+  } else if (max === g) {
     h = 60 * (2 + (b - r) / (max - min));
-  } else
-  if (max === b) {
+  } else if (max === b) {
     h = 60 * (4 + (r - g) / (max - min));
   }
 
@@ -66,26 +78,30 @@ function rgbToHsl(r, g, b) {
   if (max === 0 || min === 1) {
     s = 0;
   } else {
-    s = (max - l) / (Math.min(l, 1 - l));
+    s = (max - l) / Math.min(l, 1 - l);
   }
   // multiply s and l by 100 to get the value in percent, rather than [0,1]
   s *= 100;
   l *= 100;
 
-  return `(${parseInt(h)}, ${parseInt(s)}%, ${parseInt(l)}% )`;
+  h = parseInt(h);
+  s = parseInt(s);
+  l = parseInt(l);
+  hsl = {
+    h,
+    s,
+    l
+  };
 
+  return hsl;
 }
 
-
-
-
 function HSLToRGB(h, s, l) {
-
   s /= 100;
   l /= 100;
 
   let c = (1 - Math.abs(2 * l - 1)) * s,
-    x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
     m = l - c / 2,
     r = 0,
     g = 0,
@@ -125,18 +141,14 @@ function HSLToRGB(h, s, l) {
 
 // hsl(283, 100%, 50%)
 
-
 function RGBToHex(r, g, b) {
   r = r.toString(16);
   g = g.toString(16);
   b = b.toString(16);
 
-  if (r.length == 1)
-    r = "0" + r;
-  if (g.length == 1)
-    g = "0" + g;
-  if (b.length == 1)
-    b = "0" + b;
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
 
   return "#" + r + g + b;
 }
